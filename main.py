d645f2f4eff3376f41d7b3f3ab92c92aa5aed8de
hippyy23 <hippyy2323@gmail.com>
2026-02-09 13:20:39 +0100

    one-hot encoding; scaling; KNN model test + accuracy evaluation

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
 
 # Load data
 df = pd.read_csv('data/heart-attack-data.csv')
@@ -24,3 +28,31 @@ categorical_cols = ['cp', 'restecg', 'slope', 'thal']
 X = pd.get_dummies(X, columns=categorical_cols, drop_first=True)
 
 print(f"New shape after enconding: {X.shape}")
+
+# Train-test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+print(f"Training data: {X_train.shape}")
+print(f"Testing data: {X_test.shape}")
+
+# Feature scaling
+scaler = StandardScaler()
+
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+print("Data scaled!")
+print(f"First row of scaled training data: \n{X_train_scaled[0]}")
+
+# Model 1: KNN
+knn = KNeighborsClassifier(n_neighbors=5)
+
+# Train the model
+knn.fit(X_train_scaled, y_train)
+
+# Test the model
+y_pred_knn = knn.predict(X_test_scaled)
+
+# Evaluate the model
+accuracy = accuracy_score(y_test, y_pred_knn)
+print(f"Accuracy of KNN: {accuracy: .4f} ({accuracy*100: .2f}%)")
